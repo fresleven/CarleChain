9e308787d603d04caed58f8170f22e3d97e5ca9b
fresleven <khotayush@gmail.com>
2022-11-15 14:58:41 -0600

    Created Block Struct and implemented initialization methods

---
+++ src/blockchain.rs
@@ -0,0 +1,35 @@
+pub struct App {
+    pub blocks: Vec,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Block {
+    pub id: u64,
+    pub hash: String,
+    pub previous_hash: String,
+    pub timestamp: i64,
+    //data for each hospital patient
+    pub patient_id: u64,
+    pub age: u64,
+    pub patient_name: String,
+    pub nonce: u64,
+}
+
+impl App {
+    fn new() -> Self {
+        Self { blocks: vec![] }
+    }
+    //starting block, doesn't adhere to same rules
+    fn genesis(&mut self) {
+        let genesis_block = Block {
+            id: 0,
+            timestamp: Utc::now().timestamp(),
+            previous_hash: String::from("genesis"),
+            data: String::from("genesis!"),
+            nonce: 2836,
+            hash: "0000f816a87f806bb0073dcf026a64fb40c946b5abee2573702828694d5b4c43".to_string(),
+        };
+        self.blocks.push(genesis_block);
+    }
+
+}
