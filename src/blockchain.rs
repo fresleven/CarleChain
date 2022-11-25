1193ed49d980e89ffa20de2bd62a06431a924d90
Samuel Gerstein <samuelgerstein@Samuels-Air-6.lan>
2022-11-25 13:37:13 -0500

    Created struct def for patient, began the process of block creation, created multiple helper functions

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -1,5 +1,5 @@
 pub struct Blockchain {
-    pub blocks: Vec,
+    pub blocks: Vec<Block>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -8,28 +8,64 @@ pub struct Block {
     pub hash: String,
     pub previous_hash: String,
     pub timestamp: i64,
+    pub nonce: u64,
     //data for each hospital patient
+    pub patient_info : Patient
+}
+
+pub struct Patient {
     pub patient_id: u64,
     pub age: u64,
     pub patient_name: String,
-    pub nonce: u64,
 }
-
 impl Blockchain {
     fn new() -> Self {
         Self { blocks: vec![] }
     }
-    //starting block, doesn't adhere to same rules
-    fn genesis(&mut self) {
+    fn add_patient(&mut self, patient_id: u64, age: u64, patient_name: u64) {
+        self.add_patient_struct(Patient{patient_id, age, patient_name})
+    }
+    fn add_patient_struct(&mut self, patient: Patient) {
+        if self.blocks.is_empty() {
+            self.genesis(patient)
+        }
+        else {
+            self.add_patient_nonempty(patient)
+        }
+    }
+    fn genesis(&mut self, patient: Patient) {
         let genesis_block = Block {
             id: 0,
             timestamp: Utc::now().timestamp(),
             previous_hash: String::from("genesis"),
-            data: String::from("genesis!"),
+            patient_info: patient,
             nonce: 2836,
             hash: "0000f816a87f806bb0073dcf026a64fb40c946b5abee2573702828694d5b4c43".to_string(),
         };
         self.blocks.push(genesis_block);
     }
+    fn add_patient_nonempty(&mut self, patient: Patient) {
+        let block: Block = self.create_block(patient);
+        self.try_add_block(block)
+    }
+    fn create_block(&mut self, patient: Patient) -> Block {
+        let id = self.blocks.last().expect("Blockchain is not empty").id + 1;
+        let timestamp = Utc::now().timestamp();
+        let patient_info = patient;
+        let previous_hash = self.blocks.last().expect("Blockchain is not empty").hash;
+        let hash = generate_hash(previous_hash);
+        let nonce = generate_nonce();
+        Block {id, hash, previous_hash, timestamp, nonce, patient_info}
+    }
+    fn try_add_block(&mut self, block: Block) {
+        //Need to validate block hash/nonce/id is correct
+    }
+}
 
+fn generate_hash(previous_hash: String) -> String {
+    //Impl SHA 256 algorithm
+}
+
+fn generate_nonce() -> u64 {
+    //Impl random num generator
 }
